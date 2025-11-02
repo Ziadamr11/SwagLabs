f18a2d226d6c9711c7b66b5a5a7e14212ae91bc1
MeriamEhab <meriamehab2000@gmail.com>
2025-11-02 17:28:47 +0200

    test

--- src/test/java/Page/Login_page.java
+++ src/test/java/Page/Login_page.java
@@ -7,8 +7,8 @@ public class Login_page {
 
     WebDriver driver;
     By User_Name_Input1 = By.id("user-name");
-    By Password_Input = By.id("password");
-    By Login_button = By.id("login-button");
+    By Password_Input2 = By.id("password");
+    By Login_button1 = By.id("login-button");
 
     public Login_page(WebDriver driver){
         this.driver=driver;
@@ -17,9 +17,9 @@ public class Login_page {
         driver.findElement(User_Name_Input1).sendKeys(username);
     }
     public void Enter_password(String password){
-        driver.findElement(Password_Input).sendKeys(password);
+        driver.findElement(Password_Input2).sendKeys(password);
     }
     public void Click_login(){
-        driver.findElement(Login_button).click();
+        driver.findElement(Login_button1).click();
     }
 }
